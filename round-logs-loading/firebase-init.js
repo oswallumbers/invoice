82b486bcf3d2c98c44db0ad378f80df5ec2ab753
oswallumbers <dgour@oswallumbers.com>
2025-11-10 11:54:30 +0530

    Add files via upload

---
+++ round-logs-loading/firebase-init.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyC6hR3MK_ISzW79orveFJnPgy6QtcfXhSQ",
+    authDomain: "round-logs-list.firebaseapp.com",
+    projectId: "round-logs-list",
+    storageBucket: "round-logs-list.firebasestorage.app",
+    messagingSenderId: "882157011295",
+    appId: "1:882157011295:web:a86d4a4d02f01e081db6ea",
+    measurementId: "G-JZJL5ZR7EC"
+  };
+
