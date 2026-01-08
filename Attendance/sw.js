f4d156f16a168442a267fa2fcfa5f62b023c1145
oswallumbers <dgour@oswallumbers.com>
2026-01-08 12:11:31 +0530

    Update sw.js

--- Attendance/sw.js
+++ Attendance/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'oswal-store-v6'; // Version Update kar diya hai
+const CACHE_NAME = 'oswal-store-v7'; // Version Update kar diya hai
 const ASSETS = [
   './',
   './index.html',
@@ -36,6 +36,7 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((response) => response || fetch(e.request)),
   );
 });
+
 
 
 
